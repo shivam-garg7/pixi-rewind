dd726ef73163198975c6ae75e56c2e1911ee639a
shivam-garg7 <shivamgarg2232001@gmail.com>
2022-12-26 14:56:59 +0530

    commit

--- ass3/src/index.ts
+++ ass3/src/index.ts
@@ -9,7 +9,7 @@ export const app:Application = new game({width:innerWidth,height:innerHeight,bac
 document.body.appendChild(app.view);
  export const Gamediv:HTMLDivElement=<HTMLDivElement>document.getElementById("mainpg")
 export const app2:Application=new ass3({width:innerWidth,height:innerHeight,backgroundColor:0xff9900})
-Gamediv.appendChild(app2.view)
+// Gamediv.appendChild(app2.view)
 export const Gamediv2:HTMLDivElement=<HTMLDivElement>document.getElementById("jackpot")
 export const app3 =new Application({width:innerWidth/3,height:innerHeight/3,backgroundColor:0x99f655})
 export const count=1;
